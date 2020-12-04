c05eb7e86e35e7f472540eab6175eec9082cd5e3
Joshua Paylaga <joshuapaylaga@gmail.com>
2020-12-04 16:23:43 +0800

    Use process.env directly

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const dotenv = require('dotenv').config();
 
 module.exports = {
     mode: 'development',
@@ -23,13 +22,13 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: dotenv.parsed.APP_API_URL,
-            client_id: dotenv.parsed.APP_CLIENT_ID,
-            client_secret: dotenv.parsed.APP_CLIENT_SECRET,
-            grant_type: dotenv.parsed.APP_GRANT_TYPE,
-            google_maps_api_key: dotenv.parsed.GOOGLE_MAPS_API_KEY,
-            mock_location_lat: dotenv.parsed.MOCK_LOCATION_LAT,
-            mock_location_lng: dotenv.parsed.MOCK_LOCATION_LNG,
+            apiUrl: JSON.stringify(process.env.APP_API_URL),
+            client_id: JSON.stringify(process.env.APP_CLIENT_ID),
+            client_secret: JSON.stringify(process.env.APP_CLIENT_SECRET),
+            grant_type: JSON.stringify(process.env.APP_GRANT_TYPE),
+            google_maps_api_key: JSON.stringify(process.env.GOOGLE_MAPS_API_KEY),
+            mock_location_lat: JSON.stringify(process.env.MOCK_LOCATION_LAT),
+            mock_location_lng: JSON.stringify(process.env.MOCK_LOCATION_LNG),
         })
     }
 }
